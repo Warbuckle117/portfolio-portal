513b80ac926fd0ccb9598c3ec66ae39767f45b9a
mark.c.arbuckle.mil <mark.c.arbuckle.mil@army.mil>
2023-12-05 11:09:24 -0500

    backend/data-service is functional with express.js

---
+++ backend/data-service/src/Helpers/Types.ts
@@ -0,0 +1,6 @@
+export interface User {
+    username: string
+}
+export interface UserProps {
+    users: User[]
+}
--- backend/data-service/src/server.ts
+++ backend/data-service/src/server.ts
@@ -1,18 +1,20 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
 const app = express()
-import {User} from "./src/Helpers/Types";
+const port = process.env.PORT || 3102;
+import {User} from "./Helpers/Types";
 
 app.use(cors())
 
 const testUsers: User[] = [{username: "Mark"}, {username: "Narcy"}, {username: "Guest"}]
 
 app.get("/", (req, res) => {
-    res.json("This is the root of the server")
+    res.json("This is the root of the server.")
 })
 
 app.get("/users", (req: Request, res: Response) => {
     res.json({testUsers})
 })
 
-app.listen(3102, () => { console.log("server running on port localhost:3102")})
+app.listen(port, () => { console.log(`server running on port localhost:${port}`)})
+
